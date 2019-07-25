b8276e1e9d5a76ef0485ef55178910d4ed59f411
sondt1610 <sondt1610@gmail.com>
2019-07-26 06:35:14 +0700

    dsfsd

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
 // alert('dfsd');
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        margin: 10,
+        nav: true,
+        lazyLoad: true,
+        dots: false,
+        navText: ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            480:{
+                items:1,
+                nav:true
+            },
+            769:{
+                items: 3,
+                nav: true
+            },
+            1000:{
+                items:3,
+                nav:true,
+            }
+        },
+    })
+});
